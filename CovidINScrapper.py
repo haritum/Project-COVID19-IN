e133dc9fc020dc0b75cd90b2fa7cbd6a0224336d
haritum <tummala.ahri@gmail.com>
2020-04-04 23:56:40 +0200

    Added Flags to plot state-wide data

--- CovidINScrapper.py
+++ CovidINScrapper.py
@@ -9,9 +9,14 @@
 # Import scrapy library
 from bs4 import BeautifulSoup
 import requests
-import json
 import pandas as pd
 import time
+import matplotlib.pyplot as plt
+
+#
+# Input
+save2json = True
+plot2show = False
 
 
 #
@@ -27,6 +32,21 @@ current_d = time.strftime("%Y%m%d")
 sdtable = soup.find_all('table')[0]
 df = pd.read_html(str(sdtable))
 stateCOVID19_df = df[0]
-print(stateCOVID19_df.columns)
-stateCOVID19_json = stateCOVID19_df.to_json(r'stateCOVID19_'+current_d+'.json',orient='records')
-print (' Saved data as: stateCOVID19_'+current_dt+'.json')
+stateCOVID19_df.drop(stateCOVID19_df.tail(2).index, inplace=True)
+#print(stateCOVID19_df.columns)
+#print(stateCOVID19_df.dtypes)
+stateCOVID19_df['Total Confirmed cases (Including 65 foreign Nationals)'].replace(r"\*", "", regex=True, inplace=True)
+stateCOVID19_df['Total Confirmed cases (Including 65 foreign Nationals)'] = stateCOVID19_df['Total Confirmed cases (Including 65 foreign Nationals)'].apply(pd.to_numeric)
+stateCOVID19_df['Cured/Discharged/Migrated'] = stateCOVID19_df['Cured/Discharged/Migrated'].apply(pd.to_numeric)
+stateCOVID19_df['Death'] = stateCOVID19_df['Death'].apply(pd.to_numeric)
+print(stateCOVID19_df.dtypes)
+
+if (save2json):
+    stateCOVID19_json = stateCOVID19_df.to_json(r'stateCOVID19_'+current_d+'.json',orient='records')
+    print (' Saved data as: stateCOVID19_'+current_dt+'.json')
+
+
+# Plot State-wide da(ta
+if (plot2show):
+    stateCOVID19_df.plot(kind='bar', x='Name of State / UT', y=['Total Confirmed cases (Including 65 foreign Nationals)','Cured/Discharged/Migrated','Death'])
+    plt.show()
