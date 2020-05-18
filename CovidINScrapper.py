8bd835657110fdc12e8848d8fb480a25975e2186
haritum <tummala.ahri@gmail.com>
2020-05-18 15:05:32 +0200

    CODE: Generalize naming - No hardcoded names - Print Summary for user interaction

--- CovidINScrapper.py
+++ CovidINScrapper.py
@@ -12,41 +12,73 @@ import requests
 import pandas as pd
 import time
 import matplotlib.pyplot as plt
+import sys
 
 #
 # Input
-save2json = True
-plot2show = False
+save2json = False
+plot2show = True
+
+#
+# Development print statemnts
+debug = False
 
 
 #
 # India: state-wide COVID19 information pulled on current date-time (yyyymmdd-HHMMSS)
+print('')
+print(' >> Logging onto Ministry of Health and Foreign Affairs Website...')
 url = 'https://www.mohfw.gov.in/'
 response = requests.get(url, timeout=5)
 soup = BeautifulSoup(response.content, "html.parser")
 
 #
 # StateCOVID19 data on current date-time (YYYYMMDD-hhmmss)
+print(' >> Pulling current COVID19-INDIA State-wide data...')
 current_dt = time.strftime("%Y%m%d-%H%M%S")
 current_d = time.strftime("%Y%m%d")
 sdtable = soup.find_all('table')[0]
 df = pd.read_html(str(sdtable))
 stateCOVID19_df = df[0]
-stateCOVID19_df.drop(stateCOVID19_df.tail(2).index, inplace=True)
-#print(stateCOVID19_df.columns)
-#print(stateCOVID19_df.dtypes)
-stateCOVID19_df['Total Confirmed cases (Including 65 foreign Nationals)'].replace(r"\*", "", regex=True, inplace=True)
-stateCOVID19_df['Total Confirmed cases (Including 65 foreign Nationals)'] = stateCOVID19_df['Total Confirmed cases (Including 65 foreign Nationals)'].apply(pd.to_numeric)
-stateCOVID19_df['Cured/Discharged/Migrated'] = stateCOVID19_df['Cured/Discharged/Migrated'].apply(pd.to_numeric)
-stateCOVID19_df['Death'] = stateCOVID19_df['Death'].apply(pd.to_numeric)
-print(stateCOVID19_df.dtypes)
+
+#
+# Drop last few lines with total cases information...
+stateCOVID19_df.drop(stateCOVID19_df[33:].index, inplace=True)
+
+#
+# Clean data & Change datatypes of the # columns
+if (debug): print(stateCOVID19_df.columns)
+if (debug): print(stateCOVID19_df.dtypes)
+stateCOVID19_df[stateCOVID19_df.columns[2]].replace(r"\*", "", regex=True, inplace=True)
+stateCOVID19_df[stateCOVID19_df.columns[2]] = stateCOVID19_df[stateCOVID19_df.columns[2]].apply(pd.to_numeric)
+stateCOVID19_df[stateCOVID19_df.columns[3]] = stateCOVID19_df[stateCOVID19_df.columns[3]].apply(pd.to_numeric)
+stateCOVID19_df[stateCOVID19_df.columns[4]] = stateCOVID19_df[stateCOVID19_df.columns[4]].apply(pd.to_numeric)
+if (debug): print(stateCOVID19_df.dtypes)
+
+#
+# Count no. of states & UT together in our dataset
+nstatesuts = stateCOVID19_df[stateCOVID19_df.columns[1]].nunique()
+print(' >> [Updated] COVID19-INDIA State-wide data')
+ntotcases   = stateCOVID19_df[stateCOVID19_df.columns[2]].sum()
+nrecoveries = stateCOVID19_df[stateCOVID19_df.columns[3]].sum()
+ndeadths    = stateCOVID19_df[stateCOVID19_df.columns[4]].sum()
+
+#
+# Print out summary of the case data
+print ('    -- Summary of COVID19 situation in INDIA (date: '+current_d[6:8]+'-'+current_d[4:6]+'-'+current_d[:4]+')')
+print ('        1. # Cases:',ntotcases)
+print ('        2. # Recovered:',nrecoveries)
+print ('        3. # Deaths:',ndeadths)
+
 
 if (save2json):
+    print(' >> Saving into json file ...')
     stateCOVID19_json = stateCOVID19_df.to_json(r'stateCOVID19_'+current_d+'.json',orient='records')
     print (' Saved data as: stateCOVID19_'+current_dt+'.json')
 
 
 # Plot State-wide da(ta
 if (plot2show):
-    stateCOVID19_df.plot(kind='bar', x='Name of State / UT', y=['Total Confirmed cases (Including 65 foreign Nationals)','Cured/Discharged/Migrated','Death'])
+    print(' >> Plotting ...')
+    stateCOVID19_df.plot(kind='bar', x=stateCOVID19_df.columns[1], y=stateCOVID19_df.columns[2:5])
     plt.show()
